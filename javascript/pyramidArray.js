4565e4bc11923815e1703752232479d20b125486
sinacodes95 <sinacodes95@gmail.com>
2019-09-29 19:25:26 +0100

    Add alternate solution

--- javascript/pyramidArray.js
+++ javascript/pyramidArray.js
@@ -6,11 +6,21 @@
 // pyramid(3) => [  [1],  [1, 1],  [1, 1, 1]  ]
 // Note: the subarrays should be filled with 1s
 
-export const buildPyramid = (int) => {
-  const pyramid = [];
-  for (let i = 0; i < int; i++) {
-    pyramid.push([1]);
-    pyramid[i - 1] && pyramid[i].push(...pyramid[i - 1]);
+// solution #1
+// export const buildPyramid = (int) => {
+//   const pyramid = [];
+//   for (let i = 0; i < int; i++) {
+//     pyramid.push([1]);
+//     pyramid[i - 1] && pyramid[i].push(...pyramid[i - 1]);
+//   }
+//   return pyramid;
+// }
+
+// solution #2
+export const buildPyramid = (n) => {
+  const arr = []
+  for (let i = 1; i <= n; i++) {
+    arr.push(new Array(i).fill(1))
   }
-  return pyramid;
+  return arr
 }
